c8454cc332436bbb8350391f1d88c7248eb5f6eb
Khoironi Kurnia Syah <vinsomniace@gmail.com>
2020-07-10 07:23:30 +0800

    Update Maintenance

--- resources/views/errors/maintenance.blade.php
+++ resources/views/errors/maintenance.blade.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Maintenance - Terebinime</title>
+    <link rel="shortcut icon" href="{{ asset('img/terebi.png') }}" type="image/png">
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/main.css">
+
+<body class="bg-gray-900 leading-normal tracking-normal">
+    <div class="flex items-center justify-center h-screen">
+
+        <div class="text-white text-2xl md:text-3xl lg:text-5xl font-semibold rounded-lg p-5">
+            We'll be back soon!
+        </div>
+
+    </div>
+
+</body>
+
+</html>
